3cb19f76f78fc3bc597e50de79ee11ae91d16fc8
张嘉伟 <1176967052@qq.com>
2018-09-17 16:30:50 +0800

    init project

---
+++ src/test/java/com/zjw/personalsite/PersonalSiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zjw.personalsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalSiteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
